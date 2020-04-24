b6b32aa2dd47b8fe2d5c3bdd4fd852b1b167ed8f
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-24 14:09:54 -0500

    axios get ideas, style updates

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -9,10 +9,26 @@ class IdeasContainer extends Component {
     }
   }
 
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/ideas.json')
+    .then(response => {
+      console.log(response)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return (
+            <div className="tile" key={idea.id}>
+              <h4>{idea.title}</h4>
+              <p>{idea.body}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
