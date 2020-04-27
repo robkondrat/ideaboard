e046aaa23466501f6bf01af33dafe80afb950059
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-27 15:54:05 -0500

    ideas component

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import Idea from './Idea'
 
 class IdeasContainer extends Component {
   constructor(props) {
@@ -18,15 +19,33 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/ideas',
+      { idea: 
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
+        <button 
+          className="newIdeaButton"
+          onClick={this.addNewIdea}>
+          New Idea
+        </button>
         {this.state.ideas.map((idea) => {
           return (
-            <div className="tile" key={idea.id}>
-              <h4>{idea.title}</h4>
-              <p>{idea.body}</p>
-            </div>
+            <Idea idea={idea} key={idea.id} />
           )
         })}
       </div>
